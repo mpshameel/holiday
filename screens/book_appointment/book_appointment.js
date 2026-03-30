7725fac54a528048fb32371b085188a63141c4ce
unknown <shameelahmedmp@gmail.com>
2026-03-30 21:42:37 +0530

    changes

--- screens/book_appointment/book_appointment.js
+++ screens/book_appointment/book_appointment.js
@@ -19,6 +19,30 @@ if (openBook && bookPopup && closeBook) {
     });
 }
 
+/*** Select Time ***/
+const timeTrigger = document.getElementById("timeSelectorTrigger");
+const timePicker = document.getElementById("timePicker");
+const timeInput = document.getElementById("selectedTimeInput");
+const timeSlots = document.querySelectorAll(".time-slot");
+
+timeTrigger.addEventListener("click", (e) => {
+    e.stopPropagation();
+    timePicker.classList.toggle("show");
+});
+
+timeSlots.forEach(slot => {
+    slot.addEventListener("click", () => {
+        timeInput.value = slot.innerText;
+        timePicker.classList.remove("show");
+    });
+});
+
+document.addEventListener("click", (e) => {
+    if (!timePicker.contains(e.target) && e.target !== timeTrigger) {
+        timePicker.classList.remove("show");
+    }
+});
+
 /*** Open Thank You Popup ***/
 const thankYouPopup = document.getElementById("thankYouPopup");
 const openThankYouBtn = document.getElementById("openThankYou");
